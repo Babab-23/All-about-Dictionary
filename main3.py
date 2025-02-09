640e476453f89fa817987afbf06b28e6ea5d76a0
Adrian <you@example.com>
2025-02-09 21:01:16 +0100

    third commit

---
+++ main3.py
@@ -0,0 +1,9 @@
+country_code={"India":"0091",
+              "Australia":"0025",
+              "Nepal":"00977"}
+#search dictionary for counrty code of India 
+print("Counrty Code for India -")
+print(country_code.get("India","Not found"))
+#search dictionary for counrty code of Japan
+print("Counrty Code for Japan -")
+print(country_code.get("Japan","Not found"))
